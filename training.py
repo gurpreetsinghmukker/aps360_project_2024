14a335029662984ffbf99bdb5034cd14ca25d3a3
Gurpreet <gurpreetsinghmukker@gmail.com>
2024-08-06 23:22:30 -0400

    added training.py file that can train models with bigger batch sizes. modified gitignore to ignore mtg_mel files.

--- training.py
+++ training.py
@@ -328,15 +328,15 @@ def train_model_with_params(batch_size, learning_rate, epochs, criterion, model,
     learning_rate = learning_rate
 
     # Using the MTG Dataset
-    # train_folders = [
-    #     "00", "01", "02", "03",
-    #     "04", "05", "06", "07",
-    #     "08", "09", "10", "11",
-    #     "12", "13", "14", "15",
-    #     "16", "17", "18", "19"
-    # ]
+    train_folders = [
+        "00", "01", "02", "03",
+        "04", "05", "06", "07",
+        "08", "09", "10", "11",
+        "12", "13", "14", "15",
+        "16", "17", "18", "19"
+    ]
     
-    train_folders = ["00"]
+    # train_folders = ["00"]
     
     val_folders = [
         "20"
